ea7289e9d4bf6ff2a69a16fa36eb725a74f311c2
Attila Szabo <attila.szabo@prezi.com>
2016-04-06 22:47:30 +0200

    Bootstrap templates

--- raceresults/result/views.py
+++ raceresults/result/views.py
@@ -4,9 +4,15 @@ from .models import Race, Result
 
 def race_view(request, year, name):
     race = Race.objects.get(short_name=name, date__year=year)
-
+    results = Result.objects.filter(race=race)
+    categories = [category['category'] for category in results.values('category').distinct()]
+    results_by_category = [
+        {'results': Result.objects.filter(race=race, category=item), 'category': item } for item in categories
+    ]
     context = {
         'race': race,
-        'results' : Result.objects.filter(race=race)
+        'results' : results,
+        'results_by_category': results_by_category
     }
+
     return TemplateResponse(request, 'race_view.html', context)
