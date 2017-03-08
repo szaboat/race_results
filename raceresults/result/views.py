446434f0ac03db8d6025aa898917f61806f89b88
Attila Szabo <attila.szabo@prezi.com>
2017-03-08 20:18:22 +0100

    Filter only for 2017

--- raceresults/result/views.py
+++ raceresults/result/views.py
@@ -58,9 +58,9 @@ def races_view(request, filter=None):
     if filter == 'all':
         races = Race.objects.all().order_by('date')
     elif filter:
-        races = Race.objects.filter(type=filter.upper()).order_by('date')
+        races = Race.objects.filter(filter.upper(), date__year=2017).order_by('date')
     else:
-        races = Race.objects.filter(date__gte=datetime.date.today()).order_by('date')
+        races = Race.objects.filter(date__year=2017).order_by('date')
 
     race_types = Race.type_choices
     context = {
